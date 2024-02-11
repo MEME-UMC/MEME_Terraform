2a1e09484097e5276ec4a12b851d8eca33051da0
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-02-12 04:03:54 +0900

    :add rds_security.tf

---
+++ rds_security.tf
@@ -0,0 +1,31 @@
+//rds
+resource "aws_security_group" "rds_security" {
+  name = "rds_security"
+  description = "Security group for rds instance"
+  vpc_id = aws_vpc.side_effect.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds_security"
+  }
+}
