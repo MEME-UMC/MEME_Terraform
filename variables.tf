4cea8d1e0f2d6cbd6b81f1c8933c9c804bfccc66
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-02-12 04:04:16 +0900

    :add variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "DB_USERNAME" {}
+variable "DB_PASSWORD" {}
+variable "S3_BUCKET_NAME" {}
+variable "DB_IDENTIFIER" {}
+variable "PUBLIC_KEY" {}
+variable "CERTIFICATE_DOMAIN" {}
+variable "DNS" {}
+variable "GATEWAY_DNS" {}
