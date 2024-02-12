3ccbbfa60259f69b94e327bef7f12f7d3586d3db
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-02-12 18:03:59 +0900

    :fix deleting files for temporarily

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "DB_USERNAME" {}
-variable "DB_PASSWORD" {}
-variable "S3_BUCKET_NAME" {}
-variable "DB_IDENTIFIER" {}
-variable "PUBLIC_KEY" {}
-variable "CERTIFICATE_DOMAIN" {}
-variable "DNS" {}
-variable "GATEWAY_DNS" {}
