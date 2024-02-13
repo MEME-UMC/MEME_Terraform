e13af8a38e6a8dacca8874724ee288bf1a1f63fb
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-02-13 17:02:24 +0900

    :fix terraform.yml trying with secret-key

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "elastic-ip-auth" { # rds cluster의 writer 인스턴스 endpoint 추출 
 output "elastic-ip-service" { # rds cluster의 writer 인스턴스 endpoint 추출 (mysql 설정 및 Three-tier 연동파일에 정보 입력 필요해서 추출)
   value = aws_eip.meme_service_ec2.public_ip
 }
+
+output "secret-key"{
+  value = aws_key_pair.terraform-key-pair.public_key
+}
