3fc816c6bcdd0978ecdfc5a94aff2bde2dc24321
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-02-21 23:58:18 +0900

    :fix ec2 and vpc

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,12 @@
 resource "aws_instance" "meme_service_ec2" {
   ami = "ami-0f3a440bbcff3d043"
-  availability_zone = aws_subnet.side_effect_public_subnet1.availability_zone
+  availability_zone = aws_subnet.meme_vpc_private_subnet1.availability_zone
   instance_type = "t2.micro"
   key_name = aws_key_pair.terraform-key-pair.key_name
   vpc_security_group_ids = [
-    aws_default_security_group.side_effect_default.id,
-    aws_security_group.side_effect_bastion.id
+    aws_default_security_group.meme_vpc_default.id,
   ]
-  subnet_id = aws_subnet.side_effect_public_subnet1.id
+  subnet_id = aws_subnet.meme_vpc_public_subnet1.id
   associate_public_ip_address = true
 
   tags = {
@@ -17,14 +16,30 @@ resource "aws_instance" "meme_service_ec2" {
 
 resource "aws_instance" "meme_auth_ec2" {
   ami = "ami-0f3a440bbcff3d043"
-  availability_zone = aws_subnet.side_effect_public_subnet2.availability_zone
+  availability_zone = aws_subnet.meme_vpc_private_subnet2.availability_zone
   instance_type = "t2.micro"
   key_name = aws_key_pair.terraform-key-pair.key_name
   vpc_security_group_ids = [
-    aws_default_security_group.side_effect_default.id,
-    aws_security_group.side_effect_bastion.id
+    aws_default_security_group.meme_vpc_default.id,
   ]
-  subnet_id = aws_subnet.side_effect_public_subnet2.id
+  subnet_id = aws_subnet.meme_vpc_public_subnet2.id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "meme_auth_ec2"
+  }
+}
+
+resource "aws_instance" "bastion_host" {
+  ami = "ami-0f3a440bbcff3d043"
+  availability_zone = aws_subnet.meme_vpc_public_subnet1.availability_zone
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.terraform-key-pair.key_name
+  vpc_security_group_ids = [
+    aws_default_security_group.meme_vpc_default.id,
+    aws_security_group.meme_vpc_bastion.id
+  ]
+  subnet_id = aws_subnet.meme_vpc_public_subnet2.id
   associate_public_ip_address = true
 
   tags = {
@@ -33,14 +48,12 @@ resource "aws_instance" "meme_auth_ec2" {
 }
 
 resource "aws_eip" "meme_auth_ec2" {
-  vpc = true
   instance = aws_instance.meme_auth_ec2.id
-  depends_on = ["aws_internet_gateway.side_effect_igw"]
+  depends_on = ["aws_internet_gateway.meme_vpc_igw"]
 }
 
 resource "aws_eip" "meme_service_ec2" {
-  vpc = true
   instance = aws_instance.meme_service_ec2.id
-  depends_on = ["aws_internet_gateway.side_effect_igw"]
+  depends_on = ["aws_internet_gateway.meme_vpc_igw"]
 }
 
--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-resource "aws_vpc" "side_effect" {
+resource "aws_vpc" "meme_vpc" {
   cidr_block           = "10.10.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -13,11 +13,11 @@ resource "aws_vpc" "side_effect" {
 }
 
 resource "aws_route_table" "new_public_rtb" {
-  vpc_id = aws_vpc.side_effect.id
+  vpc_id = aws_vpc.meme_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.side_effect_igw.id
+    gateway_id = aws_internet_gateway.meme_vpc_igw.id
   }
   tags = {
     Name = "NEW-PUBLIC-RTB"
@@ -25,8 +25,8 @@ resource "aws_route_table" "new_public_rtb" {
 }
 
 // public subnets
-resource "aws_subnet" "side_effect_public_subnet1" {
-  vpc_id = aws_vpc.side_effect.id
+resource "aws_subnet" "meme_vpc_public_subnet1" {
+  vpc_id = aws_vpc.meme_vpc.id
   cidr_block = "10.10.1.0/24"
   map_public_ip_on_launch = true
   availability_zone = data.aws_availability_zones.available.names[0]
@@ -35,8 +35,8 @@ resource "aws_subnet" "side_effect_public_subnet1" {
   }
 }
 
-resource "aws_subnet" "side_effect_public_subnet2" {
-  vpc_id = aws_vpc.side_effect.id
+resource "aws_subnet" "meme_vpc_public_subnet2" {
+  vpc_id = aws_vpc.meme_vpc.id
   cidr_block = "10.10.2.0/24"
   map_public_ip_on_launch = true
   availability_zone = data.aws_availability_zones.available.names[2]
@@ -46,8 +46,8 @@ resource "aws_subnet" "side_effect_public_subnet2" {
 }
 
 // private subnets
-resource "aws_subnet" "side_effect_private_subnet1" {
-  vpc_id = aws_vpc.side_effect.id
+resource "aws_subnet" "meme_vpc_private_subnet1" {
+  vpc_id = aws_vpc.meme_vpc.id
   cidr_block = "10.10.10.0/24"
   availability_zone = data.aws_availability_zones.available.names[0]
   tags = {
@@ -55,8 +55,8 @@ resource "aws_subnet" "side_effect_private_subnet1" {
   }
 }
 
-resource "aws_subnet" "side_effect_private_subnet2" {
-  vpc_id = aws_vpc.side_effect.id
+resource "aws_subnet" "meme_vpc_private_subnet2" {
+  vpc_id = aws_vpc.meme_vpc.id
   cidr_block = "10.10.11.0/24"
   availability_zone = data.aws_availability_zones.available.names[2]
   tags = {
@@ -65,7 +65,7 @@ resource "aws_subnet" "side_effect_private_subnet2" {
 }
 
 resource "aws_subnet" "db_private_subnet1" {
-  vpc_id = aws_vpc.side_effect.id
+  vpc_id = aws_vpc.meme_vpc.id
   cidr_block = "10.10.12.0/24"
   availability_zone = data.aws_availability_zones.available.names[0]
   tags = {
@@ -74,7 +74,7 @@ resource "aws_subnet" "db_private_subnet1" {
 }
 
 resource "aws_subnet" "db_private_subnet2" {
-  vpc_id = aws_vpc.side_effect.id
+  vpc_id = aws_vpc.meme_vpc.id
   cidr_block = "10.10.13.0/24"
   availability_zone = data.aws_availability_zones.available.names[2]
   tags = {
@@ -82,64 +82,64 @@ resource "aws_subnet" "db_private_subnet2" {
   }
 }
 
-resource "aws_internet_gateway" "side_effect_igw" {
-  vpc_id = aws_vpc.side_effect.id
+resource "aws_internet_gateway" "meme_vpc_igw" {
+  vpc_id = aws_vpc.meme_vpc.id
   tags = {
     Name = "internet-gateway-terraform"
   }
 }
 
 // route to internet
-resource "aws_route" "side_effect_internet_access" {
-  route_table_id = aws_vpc.side_effect.main_route_table_id
+resource "aws_route" "meme_vpc_internet_access" {
+  route_table_id = aws_vpc.meme_vpc.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.side_effect_igw.id
+  gateway_id = aws_internet_gateway.meme_vpc_igw.id
 }
 
 // elastic ip address for NAT
-resource "aws_eip" "side_effect_nat_eip" {
-  vpc = true
-  depends_on = ["aws_internet_gateway.side_effect_igw"]
+resource "aws_eip" "meme_vpc_nat_eip" {
+  domain = vpc
+  depends_on = ["aws_internet_gateway.meme_vpc_igw"]
 }
 
 // NAT gateway
-resource "aws_nat_gateway" "side_effect_nat" {
-  allocation_id = aws_eip.side_effect_nat_eip.id
-  subnet_id = aws_subnet.side_effect_public_subnet1.id
-  depends_on = ["aws_internet_gateway.side_effect_igw"]
+resource "aws_nat_gateway" "meme_vpc_nat" {
+  allocation_id = aws_eip.meme_vpc_nat_eip.id
+  subnet_id = aws_subnet.meme_vpc_public_subnet1.id
+  depends_on = ["aws_internet_gateway.meme_vpc_igw"]
 }
 
 // private route table
-resource "aws_route_table" "side_effect_private_route_table" {
-    vpc_id = aws_vpc.side_effect.id
+resource "aws_route_table" "meme_vpc_private_route_table" {
+    vpc_id = aws_vpc.meme_vpc.id
   tags = {
     Name = "private route table by terraform"
   }
 }
 
 resource "aws_route" "private_route" {
-  route_table_id = aws_route_table.side_effect_private_route_table.id
+  route_table_id = aws_route_table.meme_vpc_private_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.side_effect_nat.id
+  nat_gateway_id = aws_nat_gateway.meme_vpc_nat.id
 }
 
 // associate subnets to route tables
-resource "aws_route_table_association" "side_effect_public_subnet1_association" {
-  subnet_id = aws_subnet.side_effect_public_subnet1.id
-  route_table_id = aws_vpc.side_effect.main_route_table_id
+resource "aws_route_table_association" "meme_vpc_public_subnet1_association" {
+  subnet_id = aws_subnet.meme_vpc_public_subnet1.id
+  route_table_id = aws_vpc.meme_vpc.main_route_table_id
 }
 
-resource "aws_route_table_association" "side_effect_public_subnet2_association" {
-  subnet_id = aws_subnet.side_effect_public_subnet2.id
-  route_table_id = aws_vpc.side_effect.main_route_table_id
+resource "aws_route_table_association" "meme_vpc_public_subnet2_association" {
+  subnet_id = aws_subnet.meme_vpc_public_subnet2.id
+  route_table_id = aws_vpc.meme_vpc.main_route_table_id
 }
 
-resource "aws_route_table_association" "side_effect_private_subnet1_association" {
-  subnet_id = aws_subnet.side_effect_private_subnet1.id
-  route_table_id = aws_route_table.side_effect_private_route_table.id
+resource "aws_route_table_association" "meme_vpc_private_subnet1_association" {
+  subnet_id = aws_subnet.meme_vpc_private_subnet1.id
+  route_table_id = aws_route_table.meme_vpc_private_route_table.id
 }
 
-resource "aws_route_table_association" "side_effect_private_subnet2_association" {
-  subnet_id = aws_subnet.side_effect_private_subnet2.id
-  route_table_id = aws_route_table.side_effect_private_route_table.id
+resource "aws_route_table_association" "meme_vpc_private_subnet2_association" {
+  subnet_id = aws_subnet.meme_vpc_private_subnet2.id
+  route_table_id = aws_route_table.meme_vpc_private_route_table.id
 }
